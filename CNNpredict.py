4b44dbeacf6c44d18eb3e2425998d566ef09628f
REN Liliang <renll204@sjtu.edu.cn>
2017-11-01 11:36:26 +0800

    fix_predict

--- CNNpredict.py
+++ CNNpredict.py
@@ -1,4 +1,3 @@
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -7,19 +6,21 @@ import numpy as np
 from numpy import *
 import torch.nn.init as init
 import cv2
+import sys
+N=1
 
-def Rfilt(im,N,w,h):	
-	temp=np.arange((2*N+1)*(2*N+1)*w*h)
-	temp=temp.reshape(w*h,(2*N+1)*(2*N+1))
-	tempy=np.mod(np.mod(temp,(2*N+1))+np.mod(temp,(h*(2*N+1)*(2*N+1)))/((2*N+1)*(2*N+1)-N),h)
-	tempx=np.mod(np.mod(temp/(2*N+1),(2*N+1))+temp/((2*N+1)*(2*N+1)*h)-N,w)
-	tempma=im[tempy,tempx]
-	tempma.reshape(w*h,(2*N+1)*(2*N+1))
-	output=np.amax(tempma,axis=1)-np.amin(tempma,axis=1)	
-	output=output.reshape(w,h)
-	output=np.transpose(output)
-	return output
-	
+def Rfilt(im,N,w,h):    
+        temp=np.arange((2*N+1)*(2*N+1)*w*h)
+        temp=temp.reshape(w*h,(2*N+1)*(2*N+1))
+        tempy=np.mod(np.mod(temp,(2*N+1))+np.mod(temp,(h*(2*N+1)*(2*N+1)))/((2*N+1)*(2*N+1)-N),h)
+        tempx=np.mod(np.mod(temp/(2*N+1),(2*N+1))+temp/((2*N+1)*(2*N+1)*h)-N,w)
+        tempma=im[tempy,tempx]
+        tempma.reshape(w*h,(2*N+1)*(2*N+1))
+        output=np.amax(tempma,axis=1)-np.amin(tempma,axis=1)    
+        output=output.reshape(w,h)
+        output=np.transpose(output)
+        return output
+        
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
@@ -34,12 +35,12 @@ class Net(nn.Module):
         self.fc1 = nn.Linear(3072,2048)
         self.BN5 =nn.BatchNorm1d(2048) 
         self.fc2 = nn.Linear(2048,11)   
-        nn.init.kaiming_uniform(self.conv1.weight)
-        nn.init.kaiming_uniform(self.conv2.weight)
-        nn.init.kaiming_uniform(self.conv3.weight)
-        nn.init.kaiming_uniform(self.conv4.weight) 
-        nn.init.kaiming_uniform(self.fc1.weight) 
-        nn.init.kaiming_uniform(self.fc2.weight)
+        # nn.init.kaiming_uniform(self.conv1.weight)
+        # nn.init.kaiming_uniform(self.conv2.weight)
+        # nn.init.kaiming_uniform(self.conv3.weight)
+        # nn.init.kaiming_uniform(self.conv4.weight) 
+        # nn.init.kaiming_uniform(self.fc1.weight) 
+        # nn.init.kaiming_uniform(self.fc2.weight)
         
 
     def forward(self, x):
@@ -49,16 +50,28 @@ class Net(nn.Module):
         x = F.relu(F.max_pool2d(self.BN4(self.conv4(x)), 2,stride=2))
         x = x.view(-1, 3072) 
         x = F.relu(self.BN5(self.fc1(x))) 
-        x = F.dropout(x, training=self.training) 
+        #x = F.dropout(x, training=self.training) 
         x = F.relu(self.fc2(x))
         return F.log_softmax(x)
 
+model = Net()
+
 imgPath="PATH"
-model= torch.load('model.pt') 
+imgPath=sys.argv[1]
+#model= torch.load('model.pt')
+model.load_state_dict(torch.load('modelSD.pt'))
+model.eval()
 img= cv2.imread(imgPath, cv2.IMREAD_GRAYSCALE)
+
 img=Rfilt(img,N,44,60)
+#cv2.imwrite("T" + str(1 % 10) + ".bmp", img)
 img= np.multiply(img, 1/255.0) 
 img = img-np.mean(img)
-outputs =model(Variable(img))
- _, predicted = torch.max(outputs.data)
- print predicted
+img=img.reshape(1,1,60,44)
+#img.dtype='double'
+print img
+#print array(img).shape
+outputs =model(Variable(torch.from_numpy(img).float(),volatile=True))
+print outputs.data
+predicted =outputs.data.max(1)[1]
+print (predicted)
